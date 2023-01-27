fd895e691a10d73809918bd5544a9d534cb32b1c
Caio Dias <93464556+caiorodias@users.noreply.github.com>
2023-01-27 14:26:05 -0300

    revisando ES6

---
+++ ES6/let_const/scripts.js
@@ -0,0 +1,30 @@
+// 1 - var, let e const
+var x = 10
+var y = 15
+
+if(y > 10) {
+    var x = 5
+    console.log(x)
+}
+
+
+let a = 10
+let b = 15
+
+if(b > 10) {
+    let a = 5
+    console.log(a)
+}
+
+for(let i = 0; i < 5; i++) {
+    console.log(i)
+}
+
+function logName() {
+    const name = 'Matheus'
+    console.log(name)
+}
+
+const name = 'Pedro'
+
+logName()
