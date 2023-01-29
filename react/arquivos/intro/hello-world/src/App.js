a5f0dcc1a37b127d463c842fadbed70a2f7c95fe
caiorodias <caiorodias@hotmail.com>
2023-01-29 10:54:12 -0300

    finalizando o 1 modulo

--- react/arquivos/intro/hello-world/src/App.js
+++ react/arquivos/intro/hello-world/src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World React!</h1>
-      
+      <h2>Emmet funcionou </h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident facere quod, corporis blanditiis distinctio debitis laboriosam veniam enim magnam? Ab temporibus voluptas quia eligendi qui rerum sunt ullam nobis corrupti.</p>
     </div>
   );
 }
